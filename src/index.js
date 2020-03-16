2190606574607e7eb076a98238ce8c6eb3b7255a
iryna.blotskaya <charlie.the.unicorn.blbl@gmail.com>
2020-03-16 22:04:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  
+
+  let res = 0
+  let result = str
+  let bracket = bracketsConfig.map( el => el.join(''))
+
+ for (let i = 0; i <= bracket.length; i++ ) {
+  bracket.forEach( el => {
+    while(result.includes(el)){ result = result.replace(el, '')}
+  })
+ }
+ 
+
+  
+  res = result == '' ? true : false
+
+  return res
+
 }
